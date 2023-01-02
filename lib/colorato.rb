9c291ffc747311a05891bb2ff8df52cc5f9bc120
John Mettraux <jmettraux@gmail.com>
2023-01-02 13:27:52 +0900

    Reorganize, maybe it could become a gem

---
+++ lib/colorato.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+
+module Colorato
+
+  VERSION = '1.0.0'
+end
+
+require 'colorato/core'
+require 'colorato/module'
+
--- lib/colorato/core.rb
+++ lib/colorato/core.rb
@@ -60,13 +60,6 @@ module Colorato
 
   def self.colours(opts={})
 
-    #opts =
-    #  case opts
-    #  when Hash then opts
-    #  when Colours, NoColours then { color: opts }
-    #  else { out: opts }
-    #  end
-
     c = nil;
       [ :color, :colour, :colors, :colours ].each do |k|
         if opts.has_key?(k); c = opts[k]; break; end
@@ -84,80 +77,5 @@ module Colorato
 
     @no_colours
   end
-
-  def self.decolour(s)
-
-    s.gsub(/\x1b\[\d+(;\d+)?m/, '')
-  end
-
-  def self.no_colour_length(s)
-
-    decolour(s).length
-  end
-
-  def self.truncate_string(s, maxlen, post='...')
-
-    ncl = no_colour_length(s)
-    r = StringIO.new
-    l = 0
-
-    s.scan(/(\x1b\[\d+(?:;\d+)?m|[^\x1b]+)/) do |ss, _|
-      if ss[0, 1] == ""
-        r << ss
-      else
-#p({ r: r.string, l: l, ssl: ss.length, maxlen: maxlen, reml: maxlen - l })
-        ss = ss[0, maxlen - l]
-        r << ss
-        l += ss.length
-        break if l >= maxlen
-      end
-    end
-
-    return r.string if l < maxlen
-
-    if post.is_a?(String)
-      r << post
-    elsif post.is_a?(Proc)
-      r << post.call(ncl, maxlen, s)
-    end
-
-    r.string
-  end
-
-  class << self
-
-    alias decolor decolour
-
-    alias bw_length no_colour_length
-    alias nocolor_length no_colour_length
-    alias no_color_length no_colour_length
-    alias nocolour_length no_colour_length
-  end
 end
-
-
-#if $0 == __FILE__
-#
-#  puts "# frozen_string_literal: true"
-#  puts
-#  puts "module Colorato; class << self"
-#  Colorato::COLOURS.each do |k, v|
-#    if v.match?(/\A\d/)
-#      puts "  def #{k}(s=nil); s ? \"[#{v}m\#{s}[0;0m\" : \"[#{v}m\"; end"
-#    else
-#      puts "  alias #{k} #{v}"
-#    end
-#  end
-#  puts "end; end"
-#  puts
-#  puts "module NoColorato; class << self"
-#  Colorato::COLOURS.each do |k, v|
-#    if v.match?(/\A\d/)
-#      puts "  def #{k}(s=nil); s ? s : ''; end"
-#    else
-#      puts "  alias #{k} #{v}"
-#    end
-#  end
-#  puts "end; end"
-#end
 
