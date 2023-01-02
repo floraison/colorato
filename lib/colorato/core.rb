68c5a52e7731006b8c679928ad074a09b93ddd46
John Mettraux <jmettraux@gmail.com>
2023-01-02 14:16:55 +0900

    Reorganize colour pairs

--- lib/colorato/core.rb
+++ lib/colorato/core.rb
@@ -5,15 +5,16 @@ module Colorato
   COLOURS = Hash[*%w[
 
     reset 0;0
+
     bright 1 dim 2 underlined 4 blink 5 reverse 7 hidden 8 strike 9 default 39
+
     black 30 red 31 green 32 yellow 33 blue 34 magenta 35 cyan 36
-    light_gray 37 dark_gray 90 light_red 91 light_green 92
-    light_yellow 93 light_blue 94 light_magenta 95 light_cyan 96 white 97
+    light_gray 37 dark_gray 90 light_red 91 light_green 92 light_yellow 93
+    light_blue 94 light_magenta 95 light_cyan 96 white 97
     bg_default 49 bg_black 40 bg_red 41 bg_green 42 bg_yellow 43 bg_blue 44
     bg_magenta 45 bg_cyan 46 bg_light_gray 47 bg_dark_gray 100
     bg_light_red 101 bg_light_green 102 bg_light_yellow 103
-    bg_light_blue 104 bg_light_magenta 105 bg_light_cyan 106
-    bg_white 107
+    bg_light_blue 104 bg_light_magenta 105 bg_light_cyan 106 bg_white 107
 
     brown yellow purple magenta dark_grey dark_gray light_grey light_gray
 
@@ -22,12 +23,13 @@ module Colorato
     rs reset
     br bright bri bright un underlined rv reverse bn blink blg bg_light_gray
     und underlined rev reverse
+
   ]].freeze
 
   class Colours
 
     Colorato::COLOURS.each do |k, v|
-      if v.match?(/\A\d/)
+      if v.match(/\A\d/) # Ruby 2.3 doesn't have String#match?
         class_eval(%{
           def #{k}(s=nil)
            s ? "[#{v}m" + s + "[0;0m" : "[#{v}m"
@@ -42,7 +44,7 @@ module Colorato
   class NoColours
 
     Colorato::COLOURS.each do |k, v|
-      if v.match?(/\A\d/)
+      if v.match(/\A\d/) # Ruby 2.3 doesn't have String#match?
         class_eval("def #{k}(s=''); s; end")
       else
         class_eval("alias #{k} #{v}")
