a6cf87bc2ad09d98ec61b36d7b2c47c94d8405bd
John Mettraux <jmettraux@gmail.com>
2024-12-31 11:34:23 +0900

    Show the colours when `ruby lib/colorato.rb`

--- lib/colorato.rb
+++ lib/colorato.rb
@@ -9,3 +9,29 @@ end
 require 'colorato/core'
 require 'colorato/more'
 
+
+if __FILE__ == $0
+
+  cs = Colorato.colours.public_methods
+    .inject([]) { |a, n|
+      m = n.to_s.match(/^bg_(.+)$/)
+      a << m[1] if m
+      a }
+
+  %w[ (default) bright dim ].each do |filter|
+
+    puts
+    puts filter
+
+    f = Colorato.colours.send(filter) rescue ''
+
+    cs.each { |n|
+      fgc = f + Colorato.colours.send(n)
+      bgc = f + Colorato.colours.send("bg_#{n}")
+      t0 = fgc + 'Text' + Colorato.colours.reset
+      t1 = bgc + '    ' + Colorato.colours.reset
+      puts "%14s: %s %s" % [ n, t0, t1 ] }
+  end
+
+  puts
+end
--- lib/colorato/core.rb
+++ lib/colorato/core.rb
@@ -12,6 +12,7 @@ module Colorato
     black 30 red 31 green 32 yellow 33 blue 34 magenta 35 cyan 36
     light_gray 37 dark_gray 90 light_red 91 light_green 92 light_yellow 93
     light_blue 94 light_magenta 95 light_cyan 96 white 97
+
     bg_default 49 bg_black 40 bg_red 41 bg_green 42 bg_yellow 43 bg_blue 44
     bg_magenta 45 bg_cyan 46 bg_light_gray 47 bg_dark_gray 100
     bg_light_red 101 bg_light_green 102 bg_light_yellow 103
